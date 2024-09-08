c871c1c4d90b26f2399f621245534c6814c734ce
JoseD <95452898+JoseD2021@users.noreply.github.com>
2024-09-08 16:53:46 -0500

    Movie fixed

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from typing import Any, Optional, Tuple
 from AVLTree import AVLTree
 from Node import Node
 import pandas as pd
-from Movie import Movie #, create_movie, create_node
-Tree = AVLTree()
+from Movie import Movie, createMovie
 
-node = Movie()
-node.createMovie("Mission: Impossible II")
 
-print(node.__repr__())
-
-node = Movie().createMovie("Gladiator")
-
-print(node.__repr__())
+#tree = AVLTree()
+#
+#for i in [80,50,20,10,34,98,67,12,4,76,3,78,90,45]:
+#    tree.insert(i)
+#
+#tree.levels_nr()
+#
+#tree.delete(50)
+#
+#tree.levels_nr()
+#node = Movie()
+#node.createMovie("Mission: Impossible II")
+# 
+#print(node.__repr__())
+# 
+node = createMovie("Mission: Impossible II")
+# 
+print(node.__str__())
--- movie.py
+++ movie.py
@@ -3,8 +3,8 @@ from Node import Node
 import pandas as pd
 
 class Movie:
-    def __init__(self, title: Optional[str] = None, worldwide_earnings: Optional[int] = None, domestic_earnings: Optional[int] = None,
-        domestic_percent: Optional[float] = None, foreign_earnings: Optional[int] = None, foreign_percent: Optional[float] = None, year: Optional[int] = None) -> None:
+    def __init__(self, title = None, worldwide_earnings = None, domestic_earnings = None,
+        domestic_percent = None, foreign_earnings = None, foreign_percent = None, year = None) -> None:
         self.title = title
         self.worldwide_earnings = worldwide_earnings
         self.domestic_earnings = domestic_earnings
@@ -13,25 +13,25 @@ class Movie:
         self.foreign_percent = foreign_percent
         self.year = year
 
-    def createMovie (self,title:str):
-        df = pd.read_csv("dataset_movies.csv")
-        movie_row = df[df['Title'] == title]
-
-        if not movie_row.empty:
-            movie_data = movie_row.iloc[0]
-            self.title = movie_data['Title'],
-            self.worldwide_earnings = movie_data['Worldwide Earnings'],
-            self.domestic_earnings = movie_data['Domestic Earnings'],
-            self.domestic_percent = movie_data['Domestic Percent Earnings'],
-            self.foreign_earnings = movie_data['Foreign Earnings'],
-            self.foreign_percent = movie_data['Foreign Percent Earnings'],
-            self.year = movie_data['Year']
-        
-        else: 
-            return None
-
     def __repr__(self):  #Esta es la forma en la que muestro el nodo
         return f"Película: ({self.title}, {self.year}, Worldwide Earnings: {self.worldwide_earnings})"
+
+def createMovie (title:str):
+    df = pd.read_csv("dataset_movies.csv")
+    movie_row = df[df['Title'] == title]
+    if not movie_row.empty:
+        movie_data = movie_row.iloc[0]
+        return Movie(
+            title=movie_data['Title'],
+            worldwide_earnings=movie_data['Worldwide Earnings'],
+            domestic_earnings=movie_data['Domestic Earnings'],
+            domestic_percent=movie_data['Domestic Percent Earnings'],
+            foreign_earnings=movie_data['Foreign Earnings'],
+            foreign_percent=movie_data['Foreign Percent Earnings'],
+            year=movie_data['Year']
+        )
+    else: 
+        return None
 
 #
 #def create_movie(title: str) -> Optional[Pelicula]:
