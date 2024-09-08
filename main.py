1e3b4187fea7a5ad1795eb427b7d5ff93d06dbe6
JoseD <95452898+JoseD2021@users.noreply.github.com>
2024-09-08 16:58:04 -0500

    Rama para Rotaciones
    en esta rama AVLTree solo compara los valores de data

--- main.py
+++ main.py
@@ -5,21 +5,13 @@ import pandas as pd
 from Movie import Movie, createMovie
 
 
-#tree = AVLTree()
-#
-#for i in [80,50,20,10,34,98,67,12,4,76,3,78,90,45]:
-#    tree.insert(i)
-#
-#tree.levels_nr()
-#
-#tree.delete(50)
-#
-#tree.levels_nr()
-#node = Movie()
-#node.createMovie("Mission: Impossible II")
-# 
-#print(node.__repr__())
-# 
-node = createMovie("Mission: Impossible II")
-# 
-print(node.__str__())
+tree = AVLTree()
+
+for i in [80,50,20,10,34,98,67,12,4,76,3,78,90,45]:
+    tree.insert(i)
+
+tree.levels_nr()
+
+tree.delete(50)
+
+tree.levels_nr()
